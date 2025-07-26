5481136789df12fc46eb1b6b9a5afc8d0bd64fc9
Semih Akın <90553714+SemihAkin685@users.noreply.github.com>
2025-07-26 06:48:39 +0300

    Add files via upload
    A simple ItemDurability plugin for PowerNukkitX

---
+++ ItemDurability/src/main/java/com/semihakin685/ItemDurability.java
@@ -0,0 +1,31 @@
+package com.semihakin685;
+
+import cn.nukkit.Player;
+import cn.nukkit.event.EventHandler;
+import cn.nukkit.event.Listener;
+import cn.nukkit.event.block.BlockBreakEvent;
+import cn.nukkit.item.Item;
+import cn.nukkit.plugin.PluginBase;
+
+public class ItemDurability extends PluginBase implements Listener {
+
+    @Override
+    public void onEnable() {
+        getLogger().info("ItemDurability aktif.");
+        getServer().getPluginManager().registerEvents(this, this);
+    }
+
+    @EventHandler
+    public void onBreak(BlockBreakEvent event) {
+        Player player = event.getPlayer();
+        Item item = player.getInventory().getItemInHand();
+
+        if (item.isTool()) {
+            int durability = item.getMaxDurability();
+            int damage = item.getDamage();
+            int remaining = durability - damage;
+
+            player.sendPopup("§aDayanıklılık: §c" + remaining);
+        }
+    }
+}
